dd53d6c3cf93c1be02c5495d72b2121a3e899492
Qurbonboyev Salimbek <matniyozovasharofat9@gmail.com>
2025-09-12 18:04:30 +0300

    Yangi masala qoshildi
    .

---
+++ 12.09.py
@@ -0,0 +1,52 @@
+# ism = "Salom"
+# print(,ism)
+# # 2
+# yosh = "17"
+# print(f"Mening yoshim {yosh}")
+
+# # 3
+# yosh = 15
+# hozirgi_yil = 2025
+# tugilgan_yil = hozirgi_yil - yosh
+# print(f"Sizning tugilgan yilingiz {tugilgan_yil}")
+
+# # 4
+# a = 5
+# b = 7
+# c = a + b
+# print(c)
+
+# # 5
+# uzunlik = 4
+# eni = 6
+# s = uzunlik * eni
+# print(f"Tortburburchakning yuzi = {s}")
+
+# 6
+# radius = 5
+# l = 2 * 3,14 * radius
+# print(f"Aylananing uzunligi = {l}")
+
+# 7
+# ism = "Salimbek"
+# yosh = 15
+# print(f"Mening ismim = {ism}")
+# print(f"Yoshim = {yosh}")
+
+# # 8
+# asos = 7
+# balandlik = 4
+# s = (asos * balandlik) / 2
+# print(f"Uchburchak yuzi = {s}")
+
+# # 9
+# son = 2
+# kavadrt = son * son
+# print(kavadrt)
+
+# 10
+# a = 3
+# b = 5
+# c = 1
+# ortqymt = a * b * c
+# print(ortqymt / 3)
