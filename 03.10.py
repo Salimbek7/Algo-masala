5eaff2302bb8a2da16b22373938352d307f9425d
Qurbonboyev Salimbek <matniyozovasharofat9@gmail.com>
2025-10-03 17:14:14 +0300

    new

---
+++ 03.10.py
@@ -0,0 +1,128 @@
+# # 1
+# a = int(input("1-Sonmi kiriting:"))
+# b = int(input("2-Sonni kiriting:"))
+# sum = a + b
+# print(f"Natija: {sum}")
+from idlelib.run import interruptible
+
+# # 2
+# I = int(input("Uzunligni kiriting:"))
+# w = int(input("Enini kiriting:"))
+# area = I * w
+# print(f"Natija:{area}")
+
+# # 3
+# x = int(input("1-sonni kiriting:"))
+# y = int(input("2-sonni kiriting:"))
+# z = int(input("3-sonni kiriting:"))
+# avg = (x+y+z)/3
+# print(f"Natija:{avg}")
+
+# # 4
+# a = int(input("1-tamon:"))
+# b = int(input("2-tamon:"))
+# p = 2 * (a+b)
+# print(f"Natija:{p}")
+
+# # 5
+# h = int(input("Soatni kiriting:"))
+# m = int(input("Daqiqani kiriting:"))
+# total_min = h * 60 + m
+# print(f"Natija:{total_min}")
+
+# # 6
+# usd = int(input("Dollor miqdorini kiriting:"))
+# sum = usd * 12000
+# print(f"Natija:{sum}")
+
+# # 7
+# a = int(input("Sonni kiriting:"))
+# b = int(input("2-sonni kiriting:"))
+# diff = abs(a - b)
+# print(f"Natija:{diff}")
+
+# # 8
+# x = int(input("sonni kiriting:"))
+# n = int(input("2-sonni kiriting:"))
+# power = x**n
+# print(f"Natija:{power}")
+
+# # 9
+# a = int(input("Sonni kiriting:"))
+# b = int(input("2-sonni kiriting:"))
+# quotient = a//b
+# print(f"Qoldiq:{quotient}")
+
+# # 10
+# a = int(input("sonni kiriting:"))
+# b = int(input("2-sonni kiriting:"))
+# a,b = b,a
+# print(f"Natija:{a,b}")
+
+# ----------------------------------------
+
+# # 1
+# total_rent = int(input("Ijara pulini kiriting:"))
+# n = int(input("Oyni kiriting:"))
+# per_month = total_rent//n
+# print(f"Har oy tolanadigan pul:{per_month}")
+
+# # 2
+# d = int(input("Masofani kiriting:"))
+# v = int(input("Tezlikni kiriting:"))
+# time = d/v
+# print(f"Bosib otish kerak bolgan vaqt: {time}")
+
+# 3
+# seats = int(input("Orindiq soni:"))
+# passengers = int(input("Yolovchi soni:"))
+# per_person = passengers//seats
+# left = passengers % seats
+# print(f"Orindiq:{per_person}")
+# print(f"Qolgan:{left}")
+
+# # 4
+# consumption = int(input("100 km uchun yoqilgi sarfi: "))
+# d = int(input("Bosilgan masofa:"))
+# fuel = consumption * d/100
+# print(f"Sarf qilingan yoqilgi:{fuel}")
+
+# # 5
+# salary = int(input("Ish haqii:"))
+# net = salary * (1-0.12)
+# print(f"Netto maosh: {net}")
+
+# # 6
+# amount = int(input("Jarima:"))
+# days = int(input("Kechikkan kun:"))
+# penalty = amount*0.001*days
+# total = amount + penalty
+# print(f"Jami jarima tolov:{total}")
+
+# # 7
+# price = int(input("Narx:"))
+# discount = int(input("Chegirma:"))
+# paid = price*(1 - discount/100)
+# print(f"Tolanadigan summa:{paid}")
+
+# # 8
+# p = int(input("Asos:"))
+# r = int(input("Yillik foiz:"))
+# t = int(input("Yillar:"))
+# tinterest = p * r/100 * t
+# total =  p + tinterest
+# print(f"Jami:{tinterest}")
+
+# # 9
+# base_qty = int(input("Gramm ingredient:"))
+# people = int(input("Odam soni:"))
+# per_person = base_qty / 4
+# needed = per_person * people
+# print(f"Kishi uchun kerakli:{per_person}")
+# print(f"Gramm:{needed}")
+
+# # 10
+# bill = int(input("Hisob kitob:"))
+# tip = bill*0.10
+# total = bill + tip
+# print(f"Jami summa:{total}")
